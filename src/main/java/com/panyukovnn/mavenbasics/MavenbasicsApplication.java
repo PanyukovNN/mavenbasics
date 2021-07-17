58b5562b651be7c139048e9ee15a4d76155a2e02
Nickolay <zylex77@gmail.com>
2021-07-17 23:41:49 +0300

    learn maven

---
+++ src/main/java/com/panyukovnn/mavenbasics/MavenbasicsApplication.java
@@ -0,0 +1,10 @@
+package com.panyukovnn.mavenbasics;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenbasicsApplication {
+
+    public static void main(String[] args) {
+        StringUtils.isBlank("hello");
+    }
+}
---
+++ src/test/java/com/panyukovnn/mavenbasics/MavenbasicsTest.java
@@ -0,0 +1,15 @@
+package com.panyukovnn.mavenbasics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MavenbasicsTest {
+
+    @Test
+    public void test() {
+        String v = "Hello";
+
+        assertEquals("Hello", v);
+    }
+}
