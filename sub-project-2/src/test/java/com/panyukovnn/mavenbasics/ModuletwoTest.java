8cea86013f7600c7cb6182251bb3c36a8c5899c9
Nickolay <zylex77@gmail.com>
2021-07-25 18:04:07 +0300

    multi-module prepare for video

--- sub-project-2/src/test/java/com/panyukovnn/mavenbasics/ModuletwoTest.java
+++ sub-project-2/src/test/java/com/panyukovnn/mavenbasics/ModuletwoTest.java
@@ -1,16 +1,23 @@
 package com.panyukovnn.mavenbasics;
 
-import com.panyukovnn.mavenbasics.model.ModuleoneEntity;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class ModuletwoTest {
 
     @Test
-    public void test() {
-        ModuleoneEntity moduleoneEntity = new ModuleoneEntity();
-        moduleoneEntity.setId(1L);
-        moduleoneEntity.setDescription("From module one");
-
-        System.out.println(moduleoneEntity);
+    public void testCommonsLang() {
+        assertTrue(StringUtils.isNotEmpty("test string"));
     }
+
+//    @Test
+//    public void testModuleoneImport() {
+//        ModuleoneEntity moduleoneEntity = new ModuleoneEntity();
+//        moduleoneEntity.setId(1L);
+//        moduleoneEntity.setDescription("From module one");
+//
+//        System.out.println(moduleoneEntity);
+//    }
 }
