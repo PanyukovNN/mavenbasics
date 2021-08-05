d3d4d94af19813897d0b43bcf165bdc1c0021135
Nickolay <zylex77@gmail.com>
2021-08-05 22:16:03 +0300

    prepare master branch

--- src/main/java/com/panyukovnn/mavenbasics/MavenbasicsApplication.java
+++ src/main/java/com/panyukovnn/mavenbasics/MavenbasicsApplication.java
@@ -2,7 +2,7 @@ package com.panyukovnn.mavenbasics;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class ModuleoneApplication {
+public class MavenbasicsApplication {
 
     public static void main(String[] args) {
         StringUtils.isBlank("hello");
